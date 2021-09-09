8f16827f688efd36c50d66eb06f48242d44a3607
Opilane TTHK <opilane@tthk.ee>
2021-09-09 11:15:05 +0300

    Add PositiveNegative

---
+++ PositiveNegative/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PositiveNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm palub kasutajal sisestada numberit -10 kuni 10
+            //programm konrollib seda numbrit
+            //kui number on positiivne, konsool kuvab "positive"
+            //kui number on negatiivne, konsool kuvab "negative"
+            //kui number on 0, siis konsool kuvab "null"
+
+            Console.WriteLine("Sisesta number -10 kuni 10");
+            int UserNumber = Convert.ToInt32(Console.ReadLine());
+
+            if(UserNumber > 0)
+            {
+                Console.WriteLine("positive");
+            }
+            else if(UserNumber < 0)
+            {
+                Console.WriteLine("negative");
+            }
+            else
+            {
+                Console.WriteLine("null");
+            }
+        }
+    }
+}
